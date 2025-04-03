5d40e26b7805590b3088c3c220725e5d655d76f5
Lucas Santino <lucassteckler1@gmail.com>
2025-04-02 21:37:28 -0300

    Update RestfulAPI

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
   port: number;
+  routes: Router;
   public_path?: string;
 }
 
@@ -10,19 +11,28 @@ export class Server {
   private app = express();
   private port: number;
   private publicPath: string;
+  private routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = 'public' } = options;
+    const { routes, port, public_path = 'public' } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
     // * Middlewares
 
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
     // * Public Folder
     this.app.use(express.static(this.publicPath));
 
+    //Routes
+    this.app.use(this.routes);
+
+    // * SPA
     this.app.get('*', (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { TodosController } from './controller';
+
+export class TodoRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const todoController = new TodosController();
+
+    router.get('/', (req, res) => todoController.getTodos(req, res));
+
+    // router.get('/:id', (req, res) => todoController.getTodoById(req, res));
+
+    router.post('/', (req, res): any => todoController.createTodo(req, res));
+    router.put('/:id', (req, res): any => todoController.updateTodo(req, res));
+    router.delete('/:id', (req, res): any =>
+      todoController.deleteTodo(req, res)
+    );
+
+    return router;
+  }
+}
